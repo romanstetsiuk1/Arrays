03d9c40897bc073250a159c5ca3347310699e101
RomanStetsiuk <romansteciuk021@gmail.com>
2018-11-22 12:06:11 +0100

    Method by create array of size N and show array in descending order

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,35 @@ public class Main {
         array[x] = tmp;
     }
 
+    //    Create array with N size, and print in descending order
+    private static int[] createArrayInDescendingOrder(int size) {
+        int newArray[] = new int[size];
+        int counter = 0;
+        for (int i = newArray.length - 1; i >= 0; i--) {
+            newArray[counter] = i;
+            counter++;
+        }
+        printArray(newArray);
+        return newArray;
+    }
+
     public static void main(String[] args) {
 
         int[] array1 = new int[]{21, 3, 2, 43, 5, 76, 89, 100, 1, -8};
 
+        System.out.println("\nTest for method - printArray");
         printArray(array1);
 
-        swap(array1,1,5);
+        System.out.println("\nTest for method - swap");
+        swap(array1, 1, 5);
         printArray(array1);
-        swap(array1,0,9);
+        swap(array1, 0, 9);
         printArray(array1);
+
+        System.out.println("\nTest for method - createArrayInDescendingOrder");
+        createArrayInDescendingOrder(10);
+        createArrayInDescendingOrder(21);
+        createArrayInDescendingOrder(5);
 
     }
 
